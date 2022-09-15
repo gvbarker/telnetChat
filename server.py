c094525fdd68a2a353cb38d66fa831f780ef39b9
gvbarker <gvbarker23@wabash.edu>
2022-09-14 21:29:22 -0400

    progress on chatroom

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from multiprocessing.connection import Listener
 import socket
 import threading
 import select
@@ -9,7 +8,22 @@ HOSTPORT = 4444
 lSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lSock.bind((HOSTADD, HOSTPORT))
 lSock.listen(3)
+clients = []
+threadLock = threading.Lock()
 
 while True:
-    conn = lSock.accept()
+    conn, add = lSock.accept()
     conn.sendall(b"Good")
+
+
+
+        #def main_thread
+        #create thread for every new client
+        #client thread allows interaction inside the server
+class serverRoom(threading.Thread):
+    def __init__(self, conn, add):
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.add = add
+    def run(self):
+        print(self.address + "has connected")
