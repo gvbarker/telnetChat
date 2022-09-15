c9d3f87e5d455e721d5ee43e4f79a5be9b5c121c
gvbarker <gvbarker23@wabash.edu>
2022-09-14 23:13:28 -0400

    Username Validation

--- server.py
+++ server.py
@@ -3,27 +3,43 @@ import threading
 import select
 import errno
 
-HOSTADD = socket.gethostbyname(socket.gethostname())
-HOSTPORT = 4444
-lSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lSock.bind((HOSTADD, HOSTPORT))
-lSock.listen(3)
-clients = []
-threadLock = threading.Lock()
+def recAll(conn): 
+    received = b""
+    while True:
+        received += conn.recv(1024)
+        endOfLine = received.find(b"\r\n")
+        if(endOfLine != -1):
+            return received[:endOfLine]
 
-while True:
-    conn, add = lSock.accept()
-    conn.sendall(b"Good")
-
-
-
-        #def main_thread
-        #create thread for every new client
-        #client thread allows interaction inside the server
 class serverRoom(threading.Thread):
     def __init__(self, conn, add):
         threading.Thread.__init__(self)
         self.conn = conn
         self.add = add
     def run(self):
-        print(self.address + "has connected")
+        while True:
+            self.conn.sendall(b"Please enter a username: ")
+            user = recAll(conn)
+            if (len(user)>15):
+                self.conn.sendall(b"Please enter a shorter username.\x0a\x0d")
+                continue
+            if (user in chatters):
+                self.conn.sendall(b"Please enter an unused username.\x0a\x0d")
+                continue
+            self.conn.sendall(b"Welcome " + user)
+            chatters[user] = add
+            print(chatters)
+            break
+
+HOSTADD = socket.gethostbyname(socket.gethostname())
+HOSTPORT = 4444
+lSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lSock.bind((HOSTADD, HOSTPORT))
+lSock.listen(3)
+chatters = {}
+threadLock = threading.Lock()
+
+while True:
+    conn, add = lSock.accept()
+    serverRoom(conn, add).start()
+
