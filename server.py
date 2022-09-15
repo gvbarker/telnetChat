144f5e21cffed9d9c4f7884974412aa6848f64e6
gvbarker <gvbarker23@wabash.edu>
2022-09-15 18:20:57 -0400

    got multi-user support working

--- server.py
+++ server.py
@@ -7,39 +7,57 @@ def recAll(conn):
     received = b""
     while True:
         received += conn.recv(1024)
+        if received == b"":
+            return b""
         endOfLine = received.find(b"\r\n")
         if(endOfLine != -1):
             return received[:endOfLine]
 
+def queryUsername(conn):
+     while True:
+            conn.sendall(b"Please enter a username: ")
+            user = recAll(conn)
+            if (len(user)>15):
+                conn.sendall(b"Please enter a shorter username.\x0a\x0d")
+                continue
+            if (user in chatters):
+                conn.sendall(b"Please enter an unused username.\x0a\x0d")
+                continue
+            return user
+
+#extension of the threading class
 class serverRoom(threading.Thread):
+    
     def __init__(self, conn, add):
         threading.Thread.__init__(self)
         self.conn = conn
         self.add = add
     def run(self):
+        print(self.add + "joined")
+        user = queryUsername(self.conn)
+        self.conn.sendall(b"Welcome " + user)
+        threadLock.acquire()
+        chatters[user] = self.conn
+        threadLock.release()
         while True:
-            self.conn.sendall(b"Please enter a username: ")
-            user = recAll(conn)
-            if (len(user)>15):
-                self.conn.sendall(b"Please enter a shorter username.\x0a\x0d")
-                continue
-            if (user in chatters):
-                self.conn.sendall(b"Please enter an unused username.\x0a\x0d")
-                continue
-            self.conn.sendall(b"Welcome " + user)
-            chatters[user] = add
-            print(chatters)
-            break
+            input = recAll(self.conn)
+            if (input==b""):
+                print(self.add + "disconnected")
+                break
+        self.conn.close()
+        self.conn.sendall(b"Goodbye " + user)
+
 
 HOSTADD = socket.gethostbyname(socket.gethostname())
 HOSTPORT = 4444
 lSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lSock.bind((HOSTADD, HOSTPORT))
-lSock.listen(3)
+lSock.listen(True)
 chatters = {}
 threadLock = threading.Lock()
 
 while True:
-    conn, add = lSock.accept()
-    serverRoom(conn, add).start()
-
+    newconn, newadd = lSock.accept()
+    print(newconn)
+    print(newadd)
+    serverRoom(newconn, newadd).start()
