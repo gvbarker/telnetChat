b8340ecbb2b9f04eb899de41854f06fc13a37837
gvbarker <gvbarker23@wabash.edu>
2022-09-16 19:32:01 -0400

    message handling progress

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import select
 import errno
-
+from queue import Queue
 def recAll(conn): 
     received = b""
     while True:
@@ -13,6 +13,13 @@ def recAll(conn):
         if(endOfLine != -1):
             return received[:endOfLine]
 
+def logShift(log, msg):
+    if(not log.full()):
+        log.put(msg)
+        return
+    log.get()
+    log.put(msg)
+
 def queryUsername(conn):
      while True:
             conn.sendall(b"Please enter a username: ")
@@ -20,30 +27,36 @@ def queryUsername(conn):
             if (len(user)>15):
                 conn.sendall(b"Please enter a shorter username.\x0a\x0d")
                 continue
-            if (user in chatters):
+            if (user in connections):
                 conn.sendall(b"Please enter an unused username.\x0a\x0d")
                 continue
             return user
 
 #extension of the threading class
 class serverRoom(threading.Thread):
-    
+
     def __init__(self, conn, add):
         threading.Thread.__init__(self)
         self.conn = conn
         self.add = add
+
     def run(self):
+
         print(self.add + "joined")
         user = queryUsername(self.conn)
-        self.conn.sendall(b"Welcome " + user)
+
         threadLock.acquire()
-        chatters[user] = self.conn
+        connections[self.conn] = (user,self.add)
+        logShift(msgLog, (user + b"has joined")) 
         threadLock.release()
         while True:
+            if ()
             input = recAll(self.conn)
             if (input==b""):
                 print(self.add + "disconnected")
                 break
+
+
         self.conn.close()
         self.conn.sendall(b"Goodbye " + user)
 
@@ -53,11 +66,11 @@ HOSTPORT = 4444
 lSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lSock.bind((HOSTADD, HOSTPORT))
 lSock.listen(True)
-chatters = {}
+connections = {}
+msgLog = Queue(maxsize=10)
 threadLock = threading.Lock()
 
 while True:
     newconn, newadd = lSock.accept()
-    print(newconn)
-    print(newadd)
+    print(newadd + " joined")
     serverRoom(newconn, newadd).start()
